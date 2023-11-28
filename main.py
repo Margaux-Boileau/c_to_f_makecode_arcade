f3362a2ba5fb8727dcc20752fac6972f7717985a
Margaux Boileau <144930772+Margaux-Boileau@users.noreply.github.com>
2023-11-28 22:36:51 +0100

    added options

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-def menu():
+def menu_numbers():
     opcio_usuari = game.ask_for_number("Celsius to Fahrenheit")
     return opcio_usuari
 
 def celsius_to_fahrenheit(n: number):
     result = ((n * (9 / 5)) + 32)
     return result
+
+def fahrenheit_to_celsius(n: number):
+    result = ((n - 32) * (9 / 5))
+    return result
+
+def menu_option():
+    option = game.ask_for_number("Select option (1 or 2)", 1)
+    return option
     
 def main():
     while (1):
-        game.show_long_text("CELSIUS TO FAHRENHEIT CALCULATOR", DialogLayout.CENTER)
-        conversion = celsius_to_fahrenheit(menu())
-        game.show_long_text(conversion, DialogLayout.TOP)
+        game.show_long_text("""TEMPERATURE CONVERSOR:\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit""",
+            DialogLayout.FULL)
+        option = menu_option()
+        if option == 1:
+            num = menu_numbers()
+            conversion = fahrenheit_to_celsius(num)
+            game.show_long_text(conversion+"ºC", DialogLayout.TOP)
+        elif option == 2:
+            num = menu_numbers()
+            conversion = celsius_to_fahrenheit(num)
+            game.show_long_text(conversion+"ºF", DialogLayout.TOP)
+        else:
+            game.show_long_text('''INVALID OPTION, SELECT:\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit''',
+            DialogLayout.FULL)
+            option = menu_option()
+
 main()
--- main.ts
+++ main.ts
@@ -1,19 +1,47 @@
-function menu(): number {
+function menu_numbers(): number {
     let opcio_usuari = game.askForNumber("Celsius to Fahrenheit")
     return opcio_usuari
 }
 
-function celsius_to_fahrenheit(n: number) {
+function celsius_to_fahrenheit(n: number): number {
     let result = n * (9 / 5) + 32
     return result
 }
 
+function fahrenheit_to_celsius(n: number): number {
+    let result = (n - 32) * (9 / 5)
+    return result
+}
+
+function menu_option(): number {
+    let option = game.askForNumber("Select option (1 or 2)", 1)
+    return option
+}
+
 function main() {
-    let conversion: any;
+    let option: number;
+    let num: number;
+    let conversion: number;
     while (1) {
-        game.showLongText("CELSIUS TO FAHRENHEIT CALCULATOR", DialogLayout.Center)
-        conversion = celsius_to_fahrenheit(menu())
-        game.showLongText(conversion, DialogLayout.Top)
+        game.showLongText(`TEMPERATURE CONVERSOR:
+1.Fahrenheit to Celsius
+2.Celsius to Fahrenheit`, DialogLayout.Full)
+        option = menu_option()
+        if (option == 1) {
+            num = menu_numbers()
+            conversion = fahrenheit_to_celsius(num)
+            game.showLongText(conversion + "ºC", DialogLayout.Top)
+        } else if (option == 2) {
+            num = menu_numbers()
+            conversion = celsius_to_fahrenheit(num)
+            game.showLongText(conversion + "ºF", DialogLayout.Top)
+        } else {
+            game.showLongText(`INVALID OPTION, SELECT:
+1.Fahrenheit to Celsius
+2.Celsius to Fahrenheit`, DialogLayout.Full)
+            option = menu_option()
+        }
+        
     }
 }
 
